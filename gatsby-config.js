b634ca5fbd66d497ce7d3e96d6d56a93efcd4883
sushi-desu <hugemerge@gmail.com>
2019-12-19 12:59:39 +0900

    fix manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,10 +61,10 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `sushidesu-blog`,
+        short_name: `sushinote`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
